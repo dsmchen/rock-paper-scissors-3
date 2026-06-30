64869134364823b0766ca82f2161807cbbd24e08
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-07-01 07:25:43 +0800

    feat: create getComputerChoice function

--- src/main.ts
+++ src/main.ts
@@ -1,1 +1,8 @@
-console.log('Hello TS!');
+type Choice = 'rock' | 'paper' | 'scissors';
+
+function getComputerChoice(): Choice {
+  const choices = ['rock', 'paper', 'scissors'] as const;
+  return choices[Math.floor(Math.random() * 3)];
+}
+
+console.log(getComputerChoice());
