409da49127ff796fb092f773f13b675ba7659607
Doris Chen <43398278+dsmchen@users.noreply.github.com>
2026-07-01 19:00:51 +0800

    feat: add logic to play a single round

--- src/main.ts
+++ src/main.ts
@@ -5,10 +5,45 @@ function getComputerChoice(): Choice {
   return choices[Math.floor(Math.random() * 3)];
 }
 
-console.log(getComputerChoice());
-
 function getHumanChoice(): string | null {
-  return prompt('Enter choice.');
+  return prompt('Enter choice: rock, paper or scissors.');
 }
 
-console.log(getHumanChoice());
+let humanScore = 0;
+let computerScore = 0;
+
+function playRound(humanChoice: string | null, computerChoice: Choice) {
+  const formattedHumanChoice = humanChoice?.toLowerCase().trim();
+
+  if (
+    formattedHumanChoice !== 'rock' &&
+    formattedHumanChoice !== 'paper' &&
+    formattedHumanChoice !== 'scissors'
+  ) {
+    console.log('Invalid choice!');
+    return;
+  }
+
+  let msg = '';
+
+  if (formattedHumanChoice === computerChoice) {
+    msg = 'You draw!';
+  } else if (
+    (computerChoice === 'rock' && formattedHumanChoice === 'scissors') ||
+    (computerChoice === 'paper' && formattedHumanChoice === 'rock') ||
+    (computerChoice === 'scissors' && formattedHumanChoice === 'paper')
+  ) {
+    msg = `You lose, ${computerChoice} beats ${formattedHumanChoice}!`;
+    computerScore++;
+  } else {
+    msg = `You win, ${formattedHumanChoice} beats ${computerChoice}!`;
+    humanScore++;
+  }
+
+  console.log(msg);
+}
+
+const humanSelection = getHumanChoice();
+const computerSelection = getComputerChoice();
+
+playRound(humanSelection, computerSelection);
